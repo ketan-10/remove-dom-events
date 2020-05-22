973432ad84dec93dad301059e1caf21f1936c198
wiwium <ketanchaudhari1998@gmail.com>
2020-05-22 19:38:09 +0530

    added a kitten image in resource folder, then log it to content log (we can show it in browser)

--- content.js
+++ content.js
@@ -8,3 +8,7 @@ console.log("hello ketan");
 chrome.runtime.onMessage.addListener((request, sender, sendResponce) => {
   console.log(request.txt);
 });
+
+const resource_image_path = "resources/download.jpg";
+const url = chrome.extension.getURL(resource_image_path);
+console.log(url);
