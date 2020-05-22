3cbaa8eeb2f3f1ffcda258529ff2dce2e9c510cd
wiwium <ketanchaudhari1998@gmail.com>
2020-05-23 03:57:22 +0530

    revort

--- content/content.js
+++ content/content.js
@@ -3,19 +3,72 @@
  * When all site is loaded content.js runs
  */
 
-console.log("hello ketan");
+let all_events = {
+  change: false,
+  click: false,
+  mouseover: false,
+  mouseout: false,
+  keydown: false,
+  load: false,
+  blur: false,
+  focus: false,
+  select: false,
+  submit: false,
+  reset: false,
+  keypress: false,
+  keyup: false,
+  mousedown: false,
+  mouseup: false,
+  mousemove: false,
+  dblclick: false,
+  error: false,
+  unload: false,
+  resize: false,
+};
 
+function setStorage(events_values) {
+  console.log("first Time extenstion open");
+  chrome.storage.local.set({ events_tobe_removed: events_values }, function () {
+    console.log("Values are updated", events_values);
+  });
+}
 
-/** When background will send a message we will recive here 
- * 
-*/
-chrome.runtime.onMessage.addListener((request, sender, sendResponce) => {
-  console.log(request.txt);
+/** When background will send a message we will recive here */
+
+// TODO : load local remove all events in local
+
+chrome.storage.local.get(["events_tobe_removed"], function (result) {
+  const res = result["events_tobe_removed"];
+
+  if (res == undefined) {
+    setStorage(all_events);
+  } else {
+    console.log(res, typeof res);
+    for (const k in res) {
+      if (res[k]) {
+        const doc_event = getEventListeners(document)[k];
+        if (doc_event) {
+          for (var i = 0; i < doc_event.length; i++) {
+            document.removeEventListener(k, doc_event[i].listener);
+          }
+        }
+
+        const wid_event = getEventListeners(window)[k];
+        if (wid_event) {
+          for (var i = 0; i < wid_event.length; i++) {
+            window.removeEventListener(k, wid_event[i].listener);
+          }
+        }
+
+        // remove for simple events;
+        window.addEventListener(
+          k,
+          function (event) {
+            event.stopPropagation();
+          },
+          true
+        );
+      }
+    }
+  }
 });
-
-
-
-const resource_image_path = "resources/download.jpg";
-const url = chrome.extension.getURL(resource_image_path);
-console.log(url);
- 
--- popup/popup.js
+++ popup/popup.js
@@ -2,10 +2,65 @@
  * This script is included in popup.js
  * This is runing in extention popup
  * You can check popup console by right clicking on popup
- * 
- * THIS SCRIPT CAN ONLY MANUPLATE POPUP CONTENT 
+ *
+ * THIS SCRIPT CAN ONLY MANUPLATE POPUP CONTENT
  */
 
-console.log("popup.js running here");
+// TODO : get from local -> disply -> if updated store in local
 
-//console.log(document);
+const eventsUL = document.getElementsByClassName("events");
+
+let all_events = {
+  change: false,
+  click: false,
+  mouseover: false,
+  mouseout: false,
+  keydown: false,
+  load: false,
+  blur: false,
+  focus: false,
+  select: false,
+  submit: false,
+  reset: false,
+  keypress: false,
+  keyup: false,
+  mousedown: false,
+  mouseup: false,
+  mousemove: false,
+  dblclick: false,
+  error: false,
+  unload: false,
+  resize: false,
+};
+
+// for debug (!dont forget to remove)
+// chrome.storage.local.set({ events_tobe_removed: all_events }, function () {
+//   console.log("Values are updated");
+// });
+
+function setStorage(events_values) {
+  chrome.storage.local.set({ events_tobe_removed: events_values }, function () {
+    console.log("Values are updated", events_values);
+  });
+}
+
+chrome.storage.local.get(["events_tobe_removed"], function (result) {
+  if (result["events_tobe_removed"] == undefined) {
+    setStorage(all_events);
+  } else {
+    disply_events(result["events_tobe_removed"]);
+  }
+});
+for (const ele of eventsUL) {
+  ele.addEventListener("click", () => {
+    all_events[ele.value] = ele.checked;
+    setStorage(all_events);
+  });
+}
+
+function disply_events(events) {
+  all_events = events;
+  for (const ele of eventsUL) {
+    ele.checked = all_events[ele.value];
+  }
+}
